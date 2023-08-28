0b63fa331f5afadfc9594e67c17716cfec47c8ea
ammablo <ammablo@gmail.com>
2023-08-27 22:56:48 -0500

    Add project files.

---
+++ AgainApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using AgainApp.Models;
+
+namespace AgainApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<AgainApp.Models.Jokes>? Jokes { get; set; }
+    }
+}
---
+++ AgainApp/Models/Jokes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgainApp.Models
+{
+    public class Jokes
+    {
+        public int Id { get; set; }
+        public string JokeQuestion  { get; set; }
+        public string JokeAnswer { get; set;}
+
+        public Jokes()
+        {
+            
+        }
+    }
+}
